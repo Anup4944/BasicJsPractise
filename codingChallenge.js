c97dc48e493f0884a7a7c5d4d8dab78e42c592c2
Anup <anuppoudel60@gmail.comm>
2021-07-27 20:45:25 +1000

    more practise

---
+++ codingChallenge.js
@@ -0,0 +1,16 @@
+// const arr = ['a','n','u','p']
+// // converting array into string
+// const str = arr.join('');
+// console.log(str)
+
+
+// getting a factorial 
+// const getFactorial = (num) => {
+//   var result = 1;
+//   for (var i = 1; i < num; i++) {
+//     result *= i;
+//   }
+//   return result;
+// };
+
+// console.log(getFactorial(10));
